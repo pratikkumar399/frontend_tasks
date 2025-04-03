c37c95a61da6ece7d492d468d2b7d70266ec48eb
pratikkumar399 <pratikkumar399@gmail.com>
2025-04-04 01:27:30 +0530

    temporary polyfill of reduce

---
+++ Javascript/pollyfils/reduce.js
@@ -0,0 +1,39 @@
+
+/**
+ * @template T, U
+ * @param {(previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U} callbackFn
+ * @param {U} [initialValue]
+ * @return {Array<U>}
+ */
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+    let array = Object(this);
+    let len = array.length;
+    let accumulator;
+
+    const noInitialValue = initialValue === undefined;
+
+    if (initialValue === undefined && len === 0) {
+        throw new TypeError("Reduce of empty array with no initial value");
+    }
+
+    if (initialValue === undefined) {
+        accumulator = array[0];
+    } else {
+        accumulator = initialValue;
+    }
+
+    let index = noInitialValue ? 1 : 0;
+
+    for (let i = index; i < len; i++) {
+        if (i in array) {
+            accumulator = callbackFn.call(this, accumulator, array[i], i, array);
+        }
+    }
+    return accumulator;
+};
+
+
+
+//test cases 
+[1, 2, 3].myReduce((prev, curr) => prev + curr, 0); // 6
+[1, 2, 3].myReduce((prev, curr) => prev + curr, 4); // 10
