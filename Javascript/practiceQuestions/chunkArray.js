6f0ee49f52c0cb541f3a668d494549761edca60c
pratikkumar399 <pratikkumar399@gmail.com>
2025-05-19 12:32:55 +0530

    chunk array

--- Javascript/practiceQuestions/chunkArray.js
+++ Javascript/practiceQuestions/chunkArray.js
@@ -1,3 +1,17 @@
-function chunkArray(string , size){
-    let arr = string.split("").reverse();
+/**
+ * @template T
+ * @param {Array<T>} array The array to process.
+ * @param {number} [size=1] The length of each chunk.
+ * @returns {Array<Array<T>>} The new array of chunks.
+ */
+export default function chunk(array, size = 1) {
+    if (size <= 0) return [];
+
+    const newArray = [];
+
+    for (let i = 0; i < array.length; i += size) {
+        newArray.push(array.slice(i, i + size));
+    }
+
+    return newArray;
 }
