6b201df8a61a8f1449846995e038cb3357eadb90
pratikkumar399 <pratikkumar399@gmail.com>
2025-06-01 01:03:35 +0530

    added call pollyfill

--- Javascript/pollyfills/call.js
+++ Javascript/pollyfills/call.js
@@ -4,5 +4,35 @@
  * @return {any}
  */
 Function.prototype.myCall = function (thisArg, ...argArray) {
-   
+
+    if (typeof this !== "function") {
+        throw new TypeError("myCall must be called on a function");
+    }
+
+    thisArg = thisArg ?? globalThis;
+
+    const fnSymbol = Symbol("tempFn");
+    thisArg[fnSymbol] = this;
+
+    // Call the function and capture the return value
+    const result = thisArg[fnSymbol](...argArray);
+
+    // Cleanup the temp function property
+    delete thisArg[fnSymbol];
+
+    return result;
 };
+
+function multiplyAge(multiplier = 1) {
+    return this.age * multiplier;
+}
+const mary = {
+    age: 21,
+};
+
+const john = {
+    age: 42,
+};
+
+console.log(multiplyAge.myCall(mary)); // 21
+console.log(multiplyAge.myCall(john, 2)); // 84
