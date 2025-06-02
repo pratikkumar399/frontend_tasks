dd4ba47abab1a6358cd5960ba4b41bd689e1b103
pratikkumar399 <pratikkumar399@gmail.com>
2025-06-02 16:44:43 +0530

    added currying

--- Javascript/currying/currying.js
+++ Javascript/currying/currying.js
@@ -1,12 +1,34 @@
-function curry(fn) {
-    return function inner(...args) {
-        if (args.length >= fn.length) {
-            return fn(...args);
-        }
-        else {
-            return function (...args2) {
-                return inner(...args, ...args2)
-            }
-        }
+
+// simple currying
+
+function add(a, b) {
+    return a + b;
+}
+
+function add2(a) {
+    return function (b) {
+        return a + b;
     }
+}
+add(3, 4);
+
+/**
+ * @param {Function} func
+ * @return {Function}
+ */
+
+export default function curry(fn) {
+    // curried is the inner function that keeps collecting arguments
+    return function curried(...args) {
+        // If enough arguments are collected, call the original function
+        if (args.length >= fn.length) {
+            return fn.apply(this, args);
+        }
+
+        // Otherwise, return a function to collect more arguments
+        return function (...nextArgs) {
+            // Combine previously collected args with the new ones
+            return curried.apply(this, [...args, ...nextArgs]);
+        };
+    };
 }
