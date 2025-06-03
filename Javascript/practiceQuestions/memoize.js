be878f9e72fbe6dc43f716eeeef60c37da707252
pratikkumar399 <pratikkumar399@gmail.com>
2025-06-03 14:57:32 +0530

    added memoize

---
+++ Javascript/practiceQuestions/memoize.js
@@ -0,0 +1,47 @@
+/*
+    
+    A memoize function is a higher-order function that takes in a function and returns a memoized version of it. 
+    The memoized function caches the results of expensive function calls and returns the cached result when it 
+    receives the same inputs again. This can significantly improve the performance of functions that involve 
+    complex processing / significant latency and are called with the same arguments repeatedly.
+
+    Memoization is a technique to cache the results of expensive function calls and 
+    return the cached result when the same inputs occur again.
+
+*/
+
+
+/**
+ * @param {Function} func
+ * @returns Function
+ */
+
+
+function memoize(func) {
+
+    const cache = new Map();
+
+    return function (...args) {
+        const key = JSON.stringify(args); // serialize the arguments as keys
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        const result = func.apply(this, args);
+        cache.set(key, result);
+        return result;
+    }
+
+}
+
+
+const slowAdd = (a, b) => {
+    console.log("Calculating...");
+    return a + b;
+};
+
+const memoizedAdd = memoize(slowAdd);
+
+console.log(memoizedAdd(2, 3)); // Logs "Calculating..." then 5
+console.log(memoizedAdd(2, 3)); // Logs 5 (from cache)
