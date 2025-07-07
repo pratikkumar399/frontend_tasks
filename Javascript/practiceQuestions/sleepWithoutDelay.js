2717738c9ae09ed4c77ee5cff510af1d11218bbb
pratikkumar399 <pratikkumar399@gmail.com>
2025-07-08 01:12:40 +0530

    added sleep

---
+++ Javascript/practiceQuestions/sleepWithoutDelay.js
@@ -0,0 +1,17 @@
+async function sleep(duration) {
+  return new Promise((resolve, reject) => {
+     setTimeout(() => {
+        resolve();
+     }, duration)
+  })
+}
+
+async function greeting() {
+  console.log('Hello!');
+  await sleep(2000);
+  console.log('Bye.'); // Only logs after 2000 milliseconds (2 seconds)
+}
+
+greeting();
+// t = 0: Hello!
+// t = 2000: Bye.
