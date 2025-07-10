2ca9e8de3825320145b9958c71240f07318cb566
pratikkumar399 <pratikkumar399@gmail.com>
2025-07-11 02:06:42 +0530

    added linked list

---
+++ DsAlgoForFrontend/Linkedlist/linkedList.js
@@ -0,0 +1,73 @@
+// Node class
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// LinkedList class
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // Add to end
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  // Add to start
+  prepend(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+  }
+
+  // Delete by value
+  delete(value) {
+    if (!this.head) return;
+
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      this.length--;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next && current.next.value !== value) {
+      current = current.next;
+    }
+
+    if (current.next) {
+      current.next = current.next.next;
+      if (!current.next) this.tail = current;
+      this.length--;
+    }
+  }
+
+  // Print list
+  print() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += current.value + " -> ";
+      current = current.next;
+    }
+    console.log(result + "null");
+  }
+}
