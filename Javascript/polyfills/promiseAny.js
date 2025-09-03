11a61b592f10c97815946814872e91b6bb15bb36
pratikkumar399 <pratikkumar399@gmail.com>
2025-09-03 15:13:27 +0530

    added promiseAny polyfill

--- Javascript/polyfills/promiseAny.js
+++ Javascript/polyfills/promiseAny.js
@@ -6,11 +6,34 @@ If any promise resolves → resolve immediately.
 If all reject → reject with AggregateError.
  */
 
+function promiseAny(iterable) {
+  return new Promise((resolve, reject) => {
+    let length = iterable.length;
+    let completed = 0;
+    let errors = [];
+
+    if (length === 0) {
+      return reject(new AggregateError([], "All promises were rejected"));
+    }
+
+    iterable.forEach((item, index) => {
+      Promise.resolve(item)
+        .then(value => resolve(value)) 
+        .catch(err => {
+          errors[index] = err;
+          completed++;
+          if (completed === length) {
+            reject(new AggregateError(errors, "All promises were rejected"));
+          }
+        });
+    });
+  });
+}
 
 
-const p0 = new Promise((resolve) => {
+const p0 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject(42);
+    resolve(42);
   }, 400);
 });
 const p1 = new Promise((resolve, reject) => {
@@ -19,9 +42,10 @@ const p1 = new Promise((resolve, reject) => {
   }, 100);
 });
 
-try {
-  await promiseAny([p0, p1]);
-} catch (err) {6
-  console.log(e instanceof AggregateError); // true
-  console.log(e.errors); // [ 42, "Err!" ]
-}
+
+const p2 = Promise.reject("err3");
+
+promiseAny([p0, p1, p2])
+  .then(console.log)   // "success"
+  .catch(console.error);
+
